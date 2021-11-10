a0a705fe4a9596c4c6314596d87ef2e4dc410280
Livingston-k <kaddulivingston@gmail.com>
2021-11-10 12:53:12 +0300

    gift exchange

---
+++ gift_exchange_mapper.py
@@ -0,0 +1,7 @@
+import random
+print("Nugostf employee gift exchange mapper")
+namein = input('Enter name your name: ')
+employees = ["Kaddu", "Smith", "Danson", "Daudi", "Brenda", "Francis", "Vincent", "Serina", "Adson","Hillary","Peter","Darius","Richard","Peterson","Oscar","Annet"]
+nameout = random.shuffle(employees)
+print(namein+" you will give a gift to "+nameout)
+k = input('press')
