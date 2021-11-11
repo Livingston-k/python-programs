0f313ba66a9a1144183f8d0610ccb10ce1e53742
Kaddu Livingstone <kaddulivingston@gmail.com>
2021-11-11 17:02:31 +0300

    Update gift_exchange_mapper.py
    gg

--- gift_exchange_mapper.py
+++ gift_exchange_mapper.py
@@ -1,7 +1,12 @@
 import random
 print("Nugostf employee gift exchange mapper")
-namein = input('Enter name your name: ')
+
 employees = ["Kaddu", "Smith", "Danson", "Daudi", "Brenda", "Francis", "Vincent", "Serina", "Adson","Hillary","Peter","Darius","Richard","Peterson","Oscar","Annet"]
-nameout = random.shuffle(employees)
-print(namein+" you will give a gift to "+nameout)
+   k = 0
+for k <= xrange(0,employees.len)
+	k+=1
+	namein = input('Enter name your name: ')
+	nameout = random.choice(employees)
+	print( f"{namein} you will give a gift to {nameout}")
+
 k = input('press')
